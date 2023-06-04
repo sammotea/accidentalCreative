f2b04c89f83cfbd741df02989c3753a89adca64c
Sam Thompson <sam.thompson.work@gmail.com>
2023-06-04 16:16:47 +0100

    [ v0.6.5.1 -- FOUC Bug ] Commenting out _document.tsx

--- src/pages/OLD_document.tsx
+++ src/pages/OLD_document.tsx
@@ -1,42 +1,42 @@
-import { NEXT_DATA } from "next/dist/shared/lib/utils";
-import classify from "@/utils/classify";
-import Document, { Html, Head, Main, NextScript } from "next/document";
+// import { NEXT_DATA } from "next/dist/shared/lib/utils";
+// import classify from "@/utils/classify";
+// import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default class MyDocument extends Document {
-    render() {
-        const currentPageNextData = this.props.__NEXT_DATA__;
-        const htmlClasses = getHtmlClasses(currentPageNextData);
-        //console.log(this.props);
+// export default class MyDocument extends Document {
+//     render() {
+//         const currentPageNextData = this.props.__NEXT_DATA__;
+//         const htmlClasses = getHtmlClasses(currentPageNextData);
+//         //console.log(this.props);
 
-        return (
-            <Html>
-                <Head />
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
-}
+//         return (
+//             <Html>
+//                 <Head />
+//                 <body>
+//                     <Main />
+//                     <NextScript />
+//                 </body>
+//             </Html>
+//         );
+//     }
+// }
 
-const getHtmlClasses = (currentPageNextData: NEXT_DATA) => {
-    let pageId = currentPageNextData.page;
-    let designFlavour =
-        currentPageNextData.props?.pageProps?.postData?.designFlavour || "";
-    const htmlClasses: string[] = [];
+// const getHtmlClasses = (currentPageNextData: NEXT_DATA) => {
+//     let pageId = currentPageNextData.page;
+//     let designFlavour =
+//         currentPageNextData.props?.pageProps?.postData?.designFlavour || "";
+//     const htmlClasses: string[] = [];
 
-    switch (pageId) {
-        case "/":
-            htmlClasses.push("s-home");
-            break;
-        default:
-            if (pageId.startsWith("/writing")) {
-                htmlClasses.push("s-writing");
-            }
-    }
+//     switch (pageId) {
+//         case "/":
+//             htmlClasses.push("s-home");
+//             break;
+//         default:
+//             if (pageId.startsWith("/writing")) {
+//                 htmlClasses.push("s-writing");
+//             }
+//     }
 
-    if (designFlavour) htmlClasses.push("s-designFlavour--" + designFlavour);
+//     if (designFlavour) htmlClasses.push("s-designFlavour--" + designFlavour);
 
-    return classify(...htmlClasses);
-};
+//     return classify(...htmlClasses);
+// };
