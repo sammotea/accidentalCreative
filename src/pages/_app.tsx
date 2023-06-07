f8aba1076efa2913b2f7587d0f7da7a45471ede1
Sam Thompson <sam.thompson.work@gmail.com>
2023-06-07 15:15:38 +0100

    [ v0.7.3 ] Add classes to HTML based on page.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,21 +5,4 @@ import classify from "@/utils/dev/classify";
 
 export default function App({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
-
-    // function getHtmlClasses(designFlavour: string = "") {
-    //     const htmlClasses: string[] = [];
-
-    //     switch (designFlavour) {
-    //         case "":
-    //             htmlClasses.push("s-home");
-    //             break;
-    //         default:
-    //             htmlClasses.push("s-writing");
-    //     }
-
-    //     if (designFlavour)
-    //         htmlClasses.push("s-designFlavour--" + designFlavour);
-
-    //     return classify(...htmlClasses);
-    // }
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,15 @@
 import { NEXT_DATA } from "next/dist/shared/lib/utils";
 import classify from "@/utils/dev/classify";
+import { useRouter } from "next/router";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
     render() {
+        const currentPage = this.props?.__NEXT_DATA__?.page;
+        const htmlClasses: string = getHtmlClasses(currentPage);
+
         return (
-            <Html>
+            <Html className={htmlClasses}>
                 <Head />
                 <body>
                     <Main />
@@ -13,5 +17,30 @@ export default class MyDocument extends Document {
                 </body>
             </Html>
         );
+
+        function getHtmlClasses(page: string = "") {
+            const htmlClasses: string[] = [];
+
+            if (page === "/") {
+                htmlClasses.push("s-home");
+            } else {
+                htmlClasses.push("s-writing");
+            }
+
+            switch (page) {
+                case "/":
+                    break;
+
+                case "/writing/covid/[postId]":
+                    htmlClasses.push("s-writing--covid");
+                    break;
+
+                case "/writing/doormageddon":
+                    htmlClasses.push("s-writing--doormageddon");
+                    break;
+            }
+
+            return classify(...htmlClasses);
+        }
     }
 }
